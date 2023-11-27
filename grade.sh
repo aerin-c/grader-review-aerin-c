f5e58279f6695cc5e3680cd124fdeaf9be13554f
aerin-c <88351294+aerin-c@users.noreply.github.com>
2023-11-27 11:05:00 -0800

    :wq

--- grade.sh
+++ grade.sh
@@ -8,9 +8,27 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+cp ./student-submission/ListExamples.java grading-area
+cp ./TestListExamples.java student-submission
+cp ./student-submission/TestListExamples.java grading-area
+javac -cp grading-area: grading-area/ListExamples.java grading-area/ListExamplesTests.java
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+set -e
+
+if [[ -e $1 ]] && [[ -f $1 ]] && [[$file == *ListExamples* ]]; then
+    echo "File is present."
+    exit 1
+fi
+
+if [ ! -f "lab/student-submission/TestListExamples.java" ]; then
+    echo "Error: TestListExamples.java is missing in your submission directory."
+    echo "Please check your files and submit again."
+    exit 1
+fi
+
+javac -cp "$CPATH" ./grading-area/student-submission/*.java
